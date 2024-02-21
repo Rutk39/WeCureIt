17b36cb1fac1442b5b60e6d29e91d11321b9b192
Lauren Hahn <Lhahn24@gwu.edu>
2024-02-20 21:28:07 -0500

    Renamed name

--- WeCureIT/WeCureIT/wsgi.py
+++ WeCureIT/WeCureIT/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WeCureIT project.
+WSGI config for WeCureIt project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeCureIT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeCureIt.settings')
 
 application = get_wsgi_application()
